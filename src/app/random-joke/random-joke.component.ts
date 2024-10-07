a1dffcf27ee3f08bfa5145b6967e999191f58c86
Andres De La Torre <delatorreandres4@gmail.com>
2024-10-07 05:25:02 -0700

    Implemented random joke

--- src/app/random-joke/random-joke.component.ts
+++ src/app/random-joke/random-joke.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { JokeItemComponent } from '../joke-item/joke-item.component';
+import { Joke } from '../../types';
+import { ApiService } from '../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-random-joke',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, JokeItemComponent, CommonModule],
   templateUrl: './random-joke.component.html',
   styleUrl: './random-joke.component.css'
 })
 export class RandomJokeComponent {
+  dadNum: number = 1;
+  joke?: Joke = undefined;
 
+  constructor(private apiService: ApiService){
+
+  }
+
+  private getRndInteger(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+  generatingJoke(){
+    this.joke = undefined;
+    var intervalId = setInterval(() => {
+      this.dadNum = this.getRndInteger(1, 5);
+    }, 50);
+
+    setTimeout(() => {
+      clearInterval(intervalId);
+      this.generateJoke();
+    }, 2000)
+    
+  }
+
+  generateJoke(){
+    this.apiService.fetchData("https://icanhazdadjoke.com").subscribe(
+      (resp:any) => {
+        this.joke = resp;
+      })
+  }
+
+  ngOnInit(){
+    this.dadNum = this.getRndInteger(1, 5);
+  }
 }
