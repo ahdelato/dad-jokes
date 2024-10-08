ff8572aa324854c8558de2a24560810170e1aa63
Andres De La Torre <delatorreandres4@gmail.com>
2024-10-08 08:12:36 -0700

    Added backend server for like counts

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+jokeLikes = {};
+
+app.get("/api/likes/:jokeId", (req, res) => {
+    if (jokeLikes.hasOwnProperty(req.params.jokeId)){
+        res.json({numLikes: jokeLikes[req.params.jokeId]})
+    }
+    else
+    {
+        res.json({numLikes: 0});
+    }
+});
+
+app.get("/api/topJokes", (req, res) => {
+    res.json({topJokes: Object.keys(jokeLikes).sort((a, b) => jokeLikes[b] - jokeLikes[a]).slice(0, 10)})
+});
+
+app.post("/api/addLike/:jokeId", (req, res) => {
+    if (jokeLikes.hasOwnProperty(req.params.jokeId)){
+        jokeLikes[req.params.jokeId]++;
+    }
+    else
+    {
+        jokeLikes[req.params.jokeId] = 1;
+    }
+})
+
+app.post("/api/removeLike/:jokeId", (req, res) => {
+    if (jokeLikes[req.params.jokeId]-- == 0){
+        delete jokeLikes.req.params.jokeId;
+    }  
+})
+
+app.listen(port);
